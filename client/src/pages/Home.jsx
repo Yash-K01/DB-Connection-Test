de6d23d47cc686f17b598e6f53d3faa17ee7817c
Yash Khartode <yashkhartode1004@gmail.com>
2026-07-27 23:34:03 +0530

    Pages Created.

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div style={{ padding: "40px" }}>
+      <h1>Add User Page</h1>
+    </div>
+  );
+}
+
+export default Home;
---
+++ client/src/pages/ViewUsers.jsx
@@ -0,0 +1,9 @@
+function ViewUsers() {
+  return (
+    <div style={{ padding: "40px" }}>
+      <h1>View Users Page</h1>
+    </div>
+  );
+}
+
+export default ViewUsers;
